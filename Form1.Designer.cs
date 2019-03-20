7b3e31f8021b5e2f454029d0909b6993d50dc574
User-PC <User-PC@USER>
2019-03-20 17:23:30 +0900

    DB(KOSPI)  - 회사명(company), 종목코드(code) 삽입

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,21 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axKHOpenAPI1 = new AxKHOpenAPILib.AxKHOpenAPI();
+            ((System.ComponentModel.ISupportInitialize)(this.axKHOpenAPI1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // axKHOpenAPI1
+            // 
+            this.axKHOpenAPI1.Enabled = true;
+            this.axKHOpenAPI1.Location = new System.Drawing.Point(12, 388);
+            this.axKHOpenAPI1.Name = "axKHOpenAPI1";
+            this.axKHOpenAPI1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axKHOpenAPI1.OcxState")));
+            this.axKHOpenAPI1.Size = new System.Drawing.Size(100, 50);
+            this.axKHOpenAPI1.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.axKHOpenAPI1);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axKHOpenAPI1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private AxKHOpenAPILib.AxKHOpenAPI axKHOpenAPI1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,19 +7,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace DBinit
 {
     public partial class Form1 : Form
     {
+        string sConString = "server=localhost;uid=sa;pwd=asd123;database=STOCK";
+        
+        SqlConnection scon;
+        SqlCommand scom;
+
         public Form1()
         {
             InitializeComponent();
+            Init();
         }
-
-        private void Form1_Load(object sender, EventArgs e)
+        public void Init()
         {
+            scon = new SqlConnection(sConString);
+            scom = new SqlCommand();
+            scom.Connection = scon;
 
+            axKHOpenAPI1.CommConnect();
+            axKHOpenAPI1.OnEventConnect += onConn;    
         }
+
+        public void onConn(Object sender, AxKHOpenAPILib._DKHOpenAPIEvents_OnEventConnectEvent e)
+        {
+            if(e.nErrCode == 0)
+            {
+                scon.Open();
+                string result = axKHOpenAPI1.GetCodeListByMarket("10");
+                string[] companys = result.Split(';');
+                foreach (string s in companys)
+                {
+                    scom.CommandText = $"insert into KOSPI values ('{axKHOpenAPI1.GetMasterCodeName(s)}','{s}')";
+                    scom.ExecuteNonQuery();
+                }
+                scon.Close();  
+            }
+        }
+
+
     }
 }
